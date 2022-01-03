aa7e555dc3166efe867f89f0bde2a995fdce7ba0
houzairkoussa <houzairmk@icloud.com>
2022-01-03 18:47:08 +0400

    ADD reducers

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// react imports
 import React from 'react';
 import ReactDOM from 'react-dom';
 
@@ -12,9 +13,17 @@ import './index.css';
 import App from './components/App';
 
 // redux reducers
+import whiteTilesReducer from "./reducers/white_tiles_reducer";
+import blackTilesReducer from "./reducers/black_tiles_reducer";
+import selectedSquareReducer from "./reducers/selected_square_reducer";
+import selectedPawnReducer from "./reducers/selected_pawn_reducer";
+
+// redux store
 const reducers = combineReducers({
-  selectedPawn: null,
-  selectedSquare: null
+  selectedPawn: selectedPawnReducer,
+  selectedSquare: selectedSquareReducer,
+  blackTiles: blackTilesReducer,
+  whiteTiles: whiteTilesReducer
 });
 
 const root = document.getElementById('root');
