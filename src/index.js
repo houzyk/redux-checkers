5dcacc422d670f7b81c342afc8e7a443dd845a93
houzairkoussa <houzairmk@icloud.com>
2022-01-03 17:18:01 +0400

    INIT redux store

--- src/index.js
+++ src/index.js
@@ -1,7 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// redux imports
+import { Provider } from 'react-redux';
+import { createStore, combineReducers } from 'redux';
+
+// styles
 import './index.css';
+
+// components
 import App from './components/App';
 
+// redux reducers
+const reducers = combineReducers({
+  selectedPawn: null,
+  selectedSquare: null
+});
+
 const root = document.getElementById('root');
-if (root) ReactDOM.render(<App />, root);
+if (root) {
+  ReactDOM.render(
+    <Provider store={createStore(reducers)}>
+      <App />
+    </Provider>
+    , root
+  );
+}
